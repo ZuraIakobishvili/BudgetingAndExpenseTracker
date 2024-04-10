73b84c08045ffd56eb18711febf3672ebed580e4
Zura Iakobishvili <iakobishvili.zura@gmail.com>
2024-04-10 18:00:15 +0400

    add test layer

---
+++ BudgetingAndExpenseTracker.Tests/Factory/RequestFactory.cs
@@ -0,0 +1,17 @@
+﻿using BudgetingAndExpenseTracker.Core.Features.Account.Login;
+namespace BudgetingAndExpenseTracker.Tests.Factory;
+public class RequestFactory
+{
+    public static LoginRequest GetLoginRequest(Action<LoginRequest> options = null)
+    {
+        LoginRequest request = new()
+        {
+            Email = "test@gmail.com",
+            Password = "Password1234"
+        };
+
+        options?.Invoke(request);
+
+        return request;
+    }
+}
---
+++ BudgetingAndExpenseTracker.Tests/Features/Account/AccountLogin/AccountLoginTest.cs
@@ -0,0 +1,27 @@
+﻿using BudgetingAndExpenseTracker.Core.Entities;
+using BudgetingAndExpenseTracker.Core.Exceptions;
+using BudgetingAndExpenseTracker.Core.Features.Account.Login;
+using FakeItEasy;
+using Microsoft.AspNetCore.Identity;
+
+namespace BudgetingAndExpenseTracker.Tests.Features.Account.AccountLogin;
+
+public class AccountLoginTest
+{
+    private ILoginService _loginService;
+    private UserManager<AppUser> _fakeUserManager;
+    [SetUp]
+    public void Setup()
+    {
+        _fakeUserManager = A.Fake<UserManager<AppUser>>();
+        _loginService = new LoginService(_fakeUserManager);
+    }
+
+    [Test]
+    public void ShouldThrowInvalidRequestExceptionIfRequestIsNull()
+    {
+        Assert.ThrowsAsync<InvalidRequestException>(
+                async () => await _loginService.LoginAsync(null)
+            );
+    }
+}
